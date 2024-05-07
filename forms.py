8c0eb8414c1426420e63c7d7600185f698060f59
Ricardo Perez <rxperez12@gmail.com>
2024-05-07 12:35:46 -0700

    Created home route and added form file

---
+++ forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import InputRequired, Length
+
+
+class RegisterForm(FlaskForm):
+    """Form for registering user"""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(max=20)]
+    )
+
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=8, max=20)]
+    )
+
+    email = EmailField(
+        "Email",
+        validators=[InputRequired(), Length(max=50)]
+    )
+
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired(), Length(30)]
+    )
+
+    last_name = StringField(
+        'Last Name',
+        validators=[InputRequired(), Length(30)]
+    )
